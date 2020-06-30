4489510f6efeae201bd8e4d8c7fee06d53e3ec03
cortezd334 <cortezd334@gmail.com>
2020-06-29 17:21:30 -0700

    Practice

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,8 @@
 class Heroine < ApplicationRecord
+    has_many :heroine_powers
+    has_many :powers, through: :heroine_powers
+    
+    validates :name, :super_name, presence: true
+
+    validates :super_name, uniqueness: true
 end
